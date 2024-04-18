3bf1e4678f11eaf9b2bdf76a4ad7bef163ceb582
Mohd Haider <haiderahmed12786@gmail.com>
2024-04-18 14:28:53 +0530

    created file

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div>
+			{/* tailwind css heading red */}
+			<h1 className="text-red-500 text-3xl">Hello Vite + React!</h1>
+		</div>
+	);
+}
+
+export default App;
