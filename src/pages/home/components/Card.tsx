09844daaecb123e3876ab3d73866db6d34b07abd
Mohd Haider <haiderahmed12786@gmail.com>
2024-04-21 22:52:43 +0530

    some changes are done

--- src/pages/home/components/Card.tsx
+++ src/pages/home/components/Card.tsx
@@ -1,26 +1,30 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
 function JobCard() {
-    return (<Card>
-        <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-            <p>Card Footer</p>
-        </CardFooter>
-    </Card>
-    );
+	return (
+		<Card>
+			<CardHeader>
+				<div className="flex">
+					{" "}
+					<CardTitle>Card Title</CardTitle>
+					<CardDescription>Card Description</CardDescription>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<p>Card Content</p>
+			</CardContent>
+			<CardFooter>
+				<p>Card Footer</p>
+			</CardFooter>
+		</Card>
+	);
 }
 
 export default JobCard;
