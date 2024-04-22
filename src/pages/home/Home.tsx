af4b82301da2c920115179ea04f92b15cfa98694
Arish <arishofficiall@gmail.com>
2024-04-22 23:55:30 +0530

    Select element error resolve

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,41 +1,19 @@
-// import {
-//     NavigationMenu,
-//     NavigationMenuContent,
-//     NavigationMenuIndicator,
-//     NavigationMenuItem,
-//     NavigationMenuLink,
-//     NavigationMenuList,
-//     NavigationMenuTrigger,
-//     NavigationMenuViewport,
-// } from "@/components/ui/navigation-menu"
-import { ComboboxDemo } from "./components/Combobox";
+//import { ComboboxDemo } from "./components/Combobox";
 import JobCard from "./components/Card";
-
+import SelectButton from "./components/Select";
 
 function Home() {
     return (
         <div>
             {/* header */}
-            <div>
-                {/* <NavigationMenu>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <NavigationMenuLink>Link</NavigationMenuLink>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu> */}
-            </div>
-
+        
             {/* Combobox */}
             <div className="grid my-8 mx-8 sm:grid-cols-5 gap-4 grid-cols-3">
-                <ComboboxDemo />
-                <ComboboxDemo />
-                <ComboboxDemo />
-                <ComboboxDemo />
-                <ComboboxDemo />
+                <SelectButton />
+                <SelectButton />
+                <SelectButton />
+                <SelectButton />
+                <SelectButton />
             </div>
 
             {/* Job Cards */}
