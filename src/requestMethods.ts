f8b84919e0843502d1b7661d0509a3ebe8a5009d
mohdhaider07 <haiderahmed12786@gmail.com>
2024-05-30 00:05:46 +0530

    some hcnagea are done

--- src/requestMethods.ts
+++ src/requestMethods.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const BASE_URL = import.meta.env.VITE_BASE_URL as string;
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://syncview-backend.onrender.com/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
