ec14a5870929f54c2887e5af88ad6f604c29154c
MAXASSASSINS <mohammedshadab23april@gmail.com>
2024-05-31 10:24:51 +0530

    fixing join room functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,6 +9,7 @@ function Home() {
   const [videoUrl, setVideoUrl] = useState<string>(
     "https://www.youtube.com/watch?v=F8bEEsCNPw8"
   );
+  const [roomId, setRoomId] = useState<string>("");
   // publicRequest.post request to the server to create the room the funtion will take string url of the video
   // and will return the roomId of the room created
   const navigate = useNavigate();
@@ -55,34 +56,22 @@ function Home() {
           >
             Create Room
           </button>
-          {/* <button
-            onClick={() => createRoom(videoUrl)}
-            className="px-4 py-2 w-full mt-4 bg-blue-500 rounded-md hover:bg-blue-600"
-          >
-            Join Room
-          </button> */}
         </div>
       </div>
       <p>OR</p>
       <div className="flex flex-col items-center justify-center w-1/3 p-4 bg-transparent rounded-lg">
         <Input
           type="text"
-          value={videoUrl}
+          value={roomId}
           onChange={(e) => {
-            setVideoUrl(e.target.value);
+            setRoomId(e.target.value);
           }}
           placeholder="Enter room id"
           className="placeholder:text-slate-300 text-white"
         />
         <div className="flex w-full gap-8">
-          {/* <button
-            onClick={() => createRoom(videoUrl)}
-            className="px-4 py-2 w-full mt-4 bg-blue-500 rounded-md hover:bg-blue-600"
-          >
-            Create Room
-          </button> */}
           <button
-            onClick={() => createRoom(videoUrl)}
+            onClick={() => navigate(`/room/${roomId}`)}
             className="px-4 py-2 w-full mt-4 bg-blue-500 rounded-md hover:bg-blue-600"
           >
             Join Room
